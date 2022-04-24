cc17e2e34dff4cf02eb213323e9d2a26734e895b
Albert Huynh <alberthuynh@alberts-mbp.lan>
2022-04-23 20:04:05 -0700

    Update mercari scraper file to write out to file

--- scrape-mercari.js
+++ scrape-mercari.js
@@ -1,7 +1,15 @@
 const axios = require("axios");
 const cheerio = require('cheerio');
+const fs = require("fs");
 
 const url = 'https://www.mercari.com/us/category/7/'
+
+const generateDataFile = (data) => {
+  fs.writeFile("itemlist.json", JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log("wrote new data to file itemlist.json")
+  })
+}
 
 axios(url)
   .then((response) => {
@@ -11,15 +19,14 @@ axios(url)
     const itemList = []
 
     $('[data-testid="ItemContainer"]').each((i, elem) => {
-        const item = {
-            name: $('[data-testid="ItemName"]', elem).text(),
-            brand: $('[data-testid="ItemBrand"]', elem).text(),
-            price: $('[data-testid="ItemPrice"]', elem).text().split(" ")[0]
-        }
-        itemList.push(item)
+      const item = {
+        name: $('[data-testid="ItemName"]', elem).text(),
+        brand: $('[data-testid="ItemBrand"]', elem).text(),
+        price: $('[data-testid="ItemPrice"]', elem).text().split(" ")[0]
+      }
+      itemList.push(item)
     })
 
-    console.log(`what is itemList: `, itemList)
-    console.log(`what is itemList.length: `, itemList.length)
+    generateDataFile(itemList)
   })
   .catch(console.error);
